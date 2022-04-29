bfbdd5c73bdaba7a3efebcd14befb0cd5fc10da4
Tasin Nur Jawad <55672990+Tmonstah@users.noreply.github.com>
2022-04-29 01:47:39 -0500

    Finished GUI
    Test Run Needed

--- augmentVideo.py
+++ augmentVideo.py
@@ -11,6 +11,9 @@ from tkVideoPlayer import TkinterVideo
 import time
 import tkinter as tk
 from tkVideoPlayer import TkinterVideo
+from ttkthemes import themed_tk as tk
+
+
 
 
 class Substitution:
@@ -29,14 +32,14 @@ class Substitution:
         # Corners: [top right, bottom right, bottim left, top left]
         return corners, ids
 
-    def setSourceVideo(self, filename):
-        self.sourceVideo = filename
+    def setSourceVideo(self, source_filename):
+        self.sourceVideo = source_filename
 
     def getSourceVideo(self):
         return self.sourceVideo
 
-    def setDestinationVideo(self, filename):
-        self.destinationVideo = filename
+    def setDestinationVideo(self, destination_filename):
+        self.destinationVideo = destination_filename
 
     def getDestinationVideo(self):
         return self.destinationVideo
@@ -56,10 +59,104 @@ def augment(src, dest, subs):
     print("=====")
     out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(
         *'MPEG'), 45, (int(video.get(3)), int(video.get(4))))
-    i=0
+
     while True:
-        if (i > 100):
+        _, imgAug = videoAug.read()
+        _, frame = video.read()
+        corners, ids = subs.findArucoMarkers(frame)
+
+        if ids is not None:
+            if len(ids) > 0:
+                
+                for corner, id in zip(corners, ids):
+                    if id == [0]:
+                        tl = int(corner[0][0][0]), int(corner[0][0][1])
+                    elif id == [1]:
+                        tr = int(corner[0][1][0]), int(corner[0][1][1])
+                    elif id == [2]:
+                        bl = int(corner[0][3][0]), int(corner[0][3][1])
+                    elif id == [3]:
+                        br = int(corner[0][2][0]), int(corner[0][2][1])
+                h, w, _ = imgAug.shape
+                pts1 = np.array([tl, tr, br, bl])
+                pts2 = np.float32([[0, 0], [w, 0], [w, h], [0, h]])
+                matrix, _ = cv2.findHomography(pts2, pts1)
+                imgOut = cv2.warpPerspective(
+                    imgAug, matrix, (frame.shape[1], frame.shape[0]))
+                cv2.fillConvexPoly(frame, pts1.astype(int), (0, 0, 0))
+                imgOut = frame + imgOut
+        out.write(imgOut)
+        key = cv2.waitKey(1)
+        if key == 27 or key == ord('q'):
+            cv2.destroyAllWindows()
             break
+
+
+def main():
+    window = tk.ThemedTk()
+    window.get_themes()
+    window.set_theme("radiance")
+    subs = Substitution()
+    window.title("Tkinter Play Videos in Video Player")
+    window.geometry("1200x700")
+    window.configure(bg='#D2D4F3')
+    size = [300,150]
+    outSize = [400,200]
+    
+    def open_file(typeOfFile):
+        if typeOfFile == "output":
+            augment(subs.getSourceVideo(), subs.getDestinationVideo(), subs)
+            videoplayerOutput = TkinterVideo(
+                master=window, scaled=True, pre_load=False)
+            print(videoplayerOutput.load(r"{}".format('output.avi')))
+            videoplayerOutput.place(relx=0.5, y=500, anchor=CENTER)
+            videoplayerOutput.set_size(outSize)
+            videoplayerOutput.play()
+        else:
+            file = askopenfile(mode='r')
+            if file is not None:
+                
+                filename = file.name
+                global videoplayer
+                videoplayer = TkinterVideo(
+                    master=window, scaled=True, pre_load=False)
+                videoplayer.load(r"{}".format(filename))
+                if typeOfFile == "source":
+                    source_filename = filename
+                    subs.setSourceVideo(source_filename)
+                    videoplayer.place(x=100, y=150)
+                    videoplayer.set_size(size)
+                elif typeOfFile == "dest":
+                    destination_filename = filename
+                    subs.setDestinationVideo(destination_filename)
+                    videoplayer.place(x=850, y=150)
+                    videoplayer.set_size(size)
+                videoplayer.play()
+
+    lbl1 = Label(window, text="Video on Video Augmentation",
+                 font="none 24 bold")
+    lbl1.config(anchor=CENTER)
+    lbl1.place(relx=0.5, y=20, anchor=CENTER)
+
+    sourceBtn = Button(window, text='Select Source Video',
+                       command=lambda: open_file("source"))
+    sourceBtn.place(x=100, y=100)
+
+    destBtn = Button(window, text='Select Destination Video',
+                     command=lambda: open_file("dest"))
+    destBtn.place(x=900, y=100)
+
+    outputBtn = Button(window, text='Generate Output Video',
+                       command=lambda: open_file("output"))
+    outputBtn.place(relx=0.5, y=300, anchor=CENTER)
+    window.mainloop()
+
+    subs = Substitution()
+    video = cv2.VideoCapture(destination_filename)
+    videoAug = cv2.VideoCapture(source_filename)
+    out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(
+        *'MPEG'), 45, (int(video.get(3)), int(video.get(4))))
+    while True:
         _, imgAug = videoAug.read()
         _, frame = video.read()
         corners, ids = subs.findArucoMarkers(frame)
@@ -84,60 +181,11 @@ def augment(src, dest, subs):
                     imgAug, matrix, (frame.shape[1], frame.shape[0]))
                 cv2.fillConvexPoly(frame, pts1.astype(int), (0, 0, 0))
                 imgOut = frame + imgOut
-        print(i)
-        i=i+1
         out.write(imgOut)
-
-
-def main():
-    window = Tk()
-    subs = Substitution()
-    window.title("Tkinter Play Videos in Video Player")
-    window.geometry("1200x1200")
-    window.configure()
-
-    def open_file(typeOfFile):
-        if typeOfFile == "output":
-            augment(subs.getSourceVideo(), subs.getDestinationVideo(), subs)
-            videoplayerOutput = TkinterVideo(
-                master=window, scaled=True, pre_load=False)
-            videoplayerOutput.load("output.avi")
-            videoplayerOutput.place(x=500, y=500)
-            videoplayerOutput.play()
-        else:
-            file = askopenfile(mode='r')
-            if file is not None:
-                global filename
-                filename = file.name
-                global videoplayer
-                videoplayer = TkinterVideo(
-                    master=window, scaled=True, pre_load=False)
-                videoplayer.load(r"{}".format(filename))
-                if typeOfFile == "source":
-                    subs.setSourceVideo(filename)
-                    videoplayer.place(x=100, y=150)
-                elif typeOfFile == "dest":
-                    subs.setDestinationVideo(filename)
-                    videoplayer.place(x=900, y=150)
-                videoplayer.play()
-
-    lbl1 = Label(window, text="Video on Video Augmentation",
-                 font="none 24 bold")
-    lbl1.config(anchor=CENTER)
-    lbl1.place(relx=0.5, y=20, anchor=CENTER)
-
-    sourceBtn = Button(window, text='Select Source Video',
-                       command=lambda: open_file("source"))
-    sourceBtn.place(x=100, y=100)
-
-    destBtn = Button(window, text='Select Destination Video',
-                     command=lambda: open_file("dest"))
-    destBtn.place(x=900, y=100)
-
-    outputBtn = Button(window, text='Generate Output Video',
-                       command=lambda: open_file("output"))
-    outputBtn.place(relx=0.5, y=300, anchor=CENTER)
-    window.mainloop()
+        key = cv2.waitKey(1)
+        if key == 27 or key == ord('q'):
+            cv2.destroyAllWindows()
+            break
 
 
 if __name__ == "__main__":
